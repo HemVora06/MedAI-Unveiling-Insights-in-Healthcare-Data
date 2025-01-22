4d312635b4685dac2c5d35f7f34666736528aad5
hem06 <hemvora1606@gmail.com>
2025-01-22 18:26:18 +0530

    PCA function successfully crafted(from scratch)

--- MedAI_Main.py
+++ MedAI_Main.py
@@ -272,15 +272,54 @@ class PCA():
         self.n_components = n_components
         self.data = data
         self.standardized_data = None
-    
-    def standardize_data(self, data, features = None):
+        self._corv_matrix = None
+        self._eigenvector = None
+        self._eigenvalue = None
+        self._total_variance = None
+        self._explained_variance_ratio = None
+        self._cumuative_explained_variance = None
+        self._selected_components = None
+        self._transformed_data = None
+
+    def standardizing_data(self, data, features = None):
         if features == None:
-            features = data.columns
+            features = data.select_dtypes(include=[np.number]).columns
             self.standardized_data = (data[features] - data[features].mean()) / data[features].std()
         else:
             self.standardized_data = (data[features] - data[features].mean()) / data[features].std()
         return self.standardized_data
-    def compute_covariance(self, data, features=None):
+    def compute_covariance(self):
+        self._corv_matrix = np.cov(self.standardized_data, rowvar = False)
+        return self._corv_matrix
+    def compute_eigen(self):
+        self._eigenvalue, self._eigenvector = np.linalg.eig(self._corv_matrix)
+        return self._eigenvalue, self._eigenvector
+    def sort_eigen(self):
+        sorted_indices = np.argsort(self._eigenvalue)[::-1]
+        self._eigenvalue = self._eigenvalue[sorted_indices]
+        self._eigenvector = self._eigenvector[:, sorted_indices]
+        return self._eigenvalue, self._eigenvector
+    def select_components(self):
+        self._total_variance = sum(self._eigenvalue)
+        self._explained_variance_ratio = self._eigenvalue / self._total_variance
+        self._cumuative_explained_variance = np.cumsum(self._explained_variance_ratio)
+        self._selected_components = self._eigenvector[:, : self.n_components]
+        return self._selected_components
+    def transform(self, data, features = None):
+        if features == None:
+            features = data.select_dtypes(include=[np.number]).columns        
+        centered_data = data[features] - data[features].mean()
+        self._transformed_data = centered_data.dot(self._selected_components)
+        return self._transformed_data
+    def fit_transform(self, data, features = None):
         if features == None:
             features = data.columns
-            
+        self.standardizing_data(data, features = None)
+        self.compute_covariance()
+        self.compute_eigen()
+        self.sort_eigen()
+        self.select_components()
+        return self.transform(data, features = None)
+
+principal_component_analysis = PCA(n_components = 3, data = cancer_data)
+print(principal_component_analysis.fit_transform(cancer_data))
