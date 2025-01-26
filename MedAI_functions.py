216b20876930b60b6dd470014856bf4530754646
hem06 <hemvora1606@gmail.com>
2025-01-26 16:13:03 +0530

    Train Test Split made from scratch

--- MedAI_functions.py
+++ MedAI_functions.py
@@ -93,3 +93,81 @@ class PCA():
         self.sort_eigen()
         self.select_components()
         return self.transform(data, features = None)
+    
+#Making Train Test Split
+class train_test_split():
+    def __init__(self, test_size = 0.25, train_size = None, shuffle = True, random_state = None):
+        self.test_size = test_size
+        self.shuffle = shuffle
+        self.random_state = random_state
+        self.train_size = train_size
+        self._n_samples = None
+        self.data = None
+        self.target = None
+
+    def data_validater(self, data, target):
+        #Check if the length of data and target matches
+        if len(data) != len(target):
+            raise ValueError("Data and Target must have the same number of rows")
+        #Check if the format of data is valid
+        if isinstance(data, pd.DataFrame):
+            self.data = data
+        elif isinstance(data, (list, np.ndarray)):
+            self.data = pd.DataFrame(data)
+        else:
+            raise ValueError("Data must be either a Dataframe or a List or a Ndarray")
+        #Check if the format of target is correct
+        if isinstance(target, (pd.Series, np.ndarray, list)):
+            self.target = pd.Series(target) if not isinstance(target, pd.Series) else target
+        else:
+            raise ValueError("Data must be either Series, Ndarray or List")
+            
+    def split(self, data, target):
+        self.data_validater(data, target)
+        self._n_samples = len(data)
+        self.indices = np.arrange(self._n_samples)
+        if self.random_state is not None:
+            np.random.seed(self.random_state)
+        if type(self.test_size) is float and self.train_size == None:
+            self._n_test = int(self.test_size * self._n_samples)
+            self._n_train = self._n_samples - self._n_test
+        elif type(self.test_size) is int and self.train_size == None:
+            self._n_test = self.test_size
+            self._n_train = self._n_samples - self._n_test
+        elif self.test_size == None and self.train_size == None:
+            self._n_test = int(self.test_size * self._n_samples)
+            self._n_train = self._n_samples - self._n_test
+        elif self.test_size == None and type(self.train_size) is float:
+            self._n_train = int(self.train_size * self._n_samples)
+            self._n_test = self._n_samples - self._n_train
+        elif self.test_size == None and type(self.train_size) is int:
+            self._n_train = self.train_size
+            self._n_test = self._n_samples - self._n_train
+        elif type(self.test_size) is float and type(self.train_size) is float:
+            self._n_test = int(self.test_size * self._n_samples)
+            self._n_train = int(self.train_size * self._n_samples)
+        elif type(self.test_size) is int and type(self.train_size) is int:
+            self._n_test = self.test_size
+            self._n_train = self.train_size
+        elif type(self.test_size) is int and type(self.train_size) is float:
+            self._n_train = int(self.train_size * self._n_samples)
+            self._n_test = self.test_size
+        elif type(self.test_size) is float and type(self.train_size) is int:
+            self._n_train = self.train_size
+            self._n_test = int(self.test_size * self._n_samples)
+        else:
+            raise ValueError("HOW???")
+        if self._n_test + self._n_train > self._n_samples:
+            raise ValueError("Test size and train size cannot be larger than the total number of samples")
+        if self.shuffle == True:
+            self.shuffled_indices = np.random.permutation(self.indices)
+            self.test_indices = self.shuffled_indices[:self._n_test]
+            self.train_indices = self.shuffled_indices[self._n_test:]
+        else:
+            self.test_indices = self.indices[:self._n_test]
+            self.train_indices = self.indices[self._n_test:]
+        self.X_train = data.iloc[self.train_indices]
+        self.y_train = target.iloc[self.train_indices]
+        self.X_test = data.iloc[self.test_indices]
+        self.y_test = target.iloc[self.test_indices]
+        return self.X_train, self.y_train, self.X_test, self.y_test
